f1a00eb6e2d97a4f1a989597cac74c6acb5b0352
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-05-29 10:59:24 +0300

    feat: Lock during nav is active

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,17 @@ new WOW().init();
 
 const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.header__nav');
+const navWrapper = document.querySelector('.header__nav-wrapper');
+
+const body = document.querySelector('body');
 
 burger.addEventListener('click', event => {
     event.preventDefault();
 
     burger.classList.toggle('header__burger--active');
     nav.classList.toggle('header__nav--active');
+    navWrapper.classList.toggle('header__nav-wrapper--active');
+
+    body.classList.toggle('lock');
+    body.classList.toggle('blackout');
 })
